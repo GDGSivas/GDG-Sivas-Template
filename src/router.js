726e1afc264e456beba7577ec5ca7d3eb1fc5a77
batuhanakin <batuhanhasanakin@gmail.com>
2020-03-04 22:56:48 +0300

    iwd web page

--- src/router.js
+++ src/router.js
@@ -5,46 +5,49 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  scrollBehavior() {
-    return { x: 0, y: 0 };
-  },
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
+    mode: 'history',
+    base: process.env.BASE_URL,
+    scrollBehavior() {
+        return { x: 0, y: 0 };
     },
-    {
-      path: '*',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('./views/About.vue')
-    },
-    {
-      path: '/team',
-      name: 'team',
-      component: () => import('./views/Team.vue')
-    },
-    {
-      path: '/events',
-      name: 'events',
-      component: () => import('./views/Events.vue')
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('./views/Contact.vue')
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '*',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () =>
+                import ('./views/About.vue')
+        },
+        {
+            path: '/team',
+            name: 'team',
+            component: () =>
+                import ('./views/Team.vue')
+        },
+        {
+            path: '/events',
+            name: 'events',
+            component: () =>
+                import ('./views/Events.vue')
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: () =>
+                import ('./views/Contact.vue')
+        }
+    ]
 })
